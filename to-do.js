94b07452f5c16e9fced7cac7ea964943f5a028d3
Ameet9087 <142583170+Ameet9087@users.noreply.github.com>
2023-11-04 22:10:36 +0530

    Add files via upload

---
+++ to-do.js
@@ -0,0 +1,42 @@
+const taskEntry = document.getElementById("taskEntry");
+const taskList = document.getElementById("task-list");
+
+function addTask() {
+    if (taskEntry.value == "") {
+        alert("Enter a Task");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = taskEntry.value;
+        taskList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "&#10060";
+        li.appendChild(span);
+        saveData();
+    }
+    taskEntry.value = " ";
+}
+
+
+taskList.addEventListener("click", function (event) {
+    if (event.target.tagName === "li") {
+        event.target.classList.toggle("checked");
+        saveData();
+    } else if (event.target.tagName === "span") {
+        event.target.parentElement.remove();
+        saveData();
+    }
+    console.log(event);
+}, false
+);
+
+
+function saveData() {
+    localStorage.setItem("my task", taskList.innerHTML);
+}
+
+
+function displyData() {
+    taskList.innerHTML = localStorage.getItem("my task");
+}
+displyData();
